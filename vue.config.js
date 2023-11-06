e8b905627d85dc3e31d6bdc4e8c330d8438381d9
web3 <web3@simonbonnin95@gmail.com>
2023-11-06 21:23:47 +0100

    Votre message de commit ici

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,26 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(glb|gltf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
