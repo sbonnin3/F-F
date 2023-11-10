f13f1556d64a3528b05db97295d6ee3d1c40674b
web3 <web3@simonbonnin95@gmail.com>
2023-11-10 13:05:01 +0100

    tel

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,3 @@
-<!-- <template>
-  <div>
-    <nav class="navbar">
-      <div class="container">
-        <a href="../" class="logo">
-          <img class="logo" src="./assets/images/Image1.png" alt="Logo">
-        </a>
-        <div class="nav-buttons">
-          <button class="buttons-nav" @click="goPage1">À propos</button>
-          <button class="buttons-nav" @click="goPage2">Activités</button>
-          <button class="buttons-nav" @click="goPage3">Planning</button>
-          <button class="buttons-nav" @click="goPage4">Prestataires</button>
-          <button class="buttons-nav" @click="goPage5">Connexion</button>
-        </div>
-      </div>
-    </nav>
-    <router-view></router-view>
-  </div>
-</template>
-
-<script>
-
-export default {
-  name: 'App',
-  methods: {
-    goPage1 () {
-      window.location.href = '../page/accueil'
-    },
-    goPage2 () {
-      window.location.href = '../page/activites'
-    },
-    goPage3 () {
-      window.location.href = '../page/planning'
-    },
-    goPage4 () {
-      window.location.href = '../page/prestataire'
-    },
-    goPage5 () {
-      window.location.href = '../page/connexion'
-    }
-  }
-}
-</script>
-
-<style>
-
-body, html {
-  background-color: whitesmoke;
-  color: black;
-  margin: 0;
-  padding: 0;
-  font-family: 'MV Boli', sans-serif;
-  z-index: 1;
-}
-
-a {
-  color: navajowhite;
-}
-
-.navbar {
-  position: fixed;
-  background-color: #000;
-  margin: 0;
-  color: #fff;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  z-index: 1000;
-  width: 100%;
-}
-
-.container {
-  display: flex;
-  align-items: center;
-}
-
-.fond {
-  background: black;
-}
-
-.logo {
-  margin: 0;
-  padding: 0;
-  width: 100px;
-}
-
-.nav-buttons {
-  display: flex;
-  padding-right: 50%;
-}
-
-.buttons-nav {
-  margin-left: 20%;
-  margin-right: 50%;
-}
-
-.nav-buttons button {
-  background-color: #000;
-  color: #ff0;
-  border: 2px solid #ff0;
-  border-radius: 20px;
-  padding: 3% 10%;
-  cursor: pointer;
-  font-size: 70%;
-  font-weight: bold;
-  outline: none;
-}
-
-.nav-buttons button:hover {
-  background-color: #ff0;
-  color: #000;
-}
-
-img {
-  border-radius: 20px;
-}
-
-@media screen and (max-width: 768px) {
-  .nav-buttons button {
-    padding: 2% 8%; /* Ajustez selon vos préférences */
-    margin-right: 5px; /* Ajustez selon vos préférences */
-  }
-}
-</style> -->
-
 <template>
   <div>
     <nav class="navbar">
@@ -144,73 +18,54 @@ img {
   </div>
 </template>
 
-
 <style>
-
-body, html {
-  margin: 0;
-  padding: 0;
-}
-
-.navbar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #000;
-  padding: 10px;
-  z-index: 1000;
-}
-
-.container {
-  display: flex;
-  align-items: center;
-}
-
-.content {
-  padding-top: 70px;
-}
-
-.logo img {
-  max-width: 100px;
-}
-
-.nav-buttons {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-}
-
-.buttons-nav {
-  background-color: #555;
-  color: #fff;
-  border: 2px solid #ff0;
-  border-radius: 20px;
-  padding: 10px 20px;
-  cursor: pointer;
-  font-size: 70%;
-  font-weight: bold;
-  outline: none;
-}
-
-.buttons-nav:not(:last-child) {
-  margin-right: 10px; /* Espacement entre les boutons */
-}
-
-.buttons-nav:hover {
-  background-color: #ff0;
-  color: #000;
-}
-
-@media screen and (max-width: 768px) {
-  .buttons-nav {
-    padding: 8px 16px;
-    margin-left: 10px;
+  /* Reset du style par défaut et ajout de styles de base */
+  body, h1, h2, h3, p, ul, li {
+    margin: 0;
+    padding: 0;
   }
-}
 
+  body {
+    font-family: 'Arial', sans-serif;
+  }
+
+  /* Style de la navbar */
+  .navbar {
+    background-color: #000;
+    padding: 15px 0;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo img {
+    width: 100px; /* Ajustez la largeur selon vos besoins */
+  }
+
+  /* Style des boutons de navigation */
+  .nav-buttons {
+    display: flex;
+    gap: 20px;
+  }
+
+  .buttons-nav {
+    text-decoration: none;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+  }
+
+  .buttons-nav:hover {
+    background-color: #333;
+  }
+
+  /* Style du contenu de la page */
+  router-view {
+    margin-top: 20px;
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,3 +9,22 @@ export default {
   name: 'HomeView'
 }
 </script>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden; /* Empêche le défilement de la page */
+}
+
+.content {
+  width: 100%;
+  height: 100vh; /* Assurez-vous que la hauteur de votre contenu occupe toute la vue de la fenêtre */
+  overflow: hidden;
+  background: url("../assets/images/video.gif") center center fixed;
+  background-size: cover;
+}
+
+.content h1 {
+  color: white; /* Ajoutez une couleur de texte appropriée pour être visible sur le fond */
+}
+</style>
