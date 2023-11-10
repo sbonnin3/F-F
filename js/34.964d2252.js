817fd383e57042f63b1048b7f31bca4b3740f189
web3 <web3@simonbonnin95@gmail.com>
2023-11-11 00:28:47 +0100

    Initial dist subtree commit

---
+++ js/34.964d2252.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkf_f"]=self["webpackChunkf_f"]||[]).push([[34],{34:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var s=function(){var t=this;t._self._c;return t._m(0)},r=[function(){var t=this,e=t._self._c;return e("div",{staticClass:"content"},[e("h1",[t._v("Vous êtes sur la page : Prestataire")])])}],u={name:"Prestataire"},a=u,f=n(1),l=(0,f.Z)(a,s,r,!1,null,null,null),c=l.exports}}]);
+//# sourceMappingURL=34.964d2252.js.map
