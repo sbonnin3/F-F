b2e3bfad9f48250c1b3bb6230e4fc6ac282ecd6d
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-11-14 17:14:42 +0100

    separation de la NavBar dans un composant

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,12 @@
 <template>
   <div>
-    <nav class="navbar">
-      <div class="container">
-        <div class="mobile-header">
-          <a href="../../" class="logo">
-            <img src="./assets/images/Image1.png" alt="Logo">
-          </a>
-          <button class="menu-button" @click="toggleMenu">&#9776;</button>
-        </div>
-        <div class="nav-buttons" :class="{ 'active': isMenuOpen }">
-          <router-link to="/page/accueil" class="buttons-nav">Informations</router-link>
-          <router-link to="/page/activites" class="buttons-nav">Activités</router-link>
-          <router-link to="/page/planning" class="buttons-nav">Planning</router-link>
-          <router-link to="/page/prestataire" class="buttons-nav">Prestataires</router-link>
-          <router-link to="/page/connexion" class="buttons-nav">Connexion</router-link>
-        </div>
-      </div>
-    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    return {
-      isMenuOpen: false
-    };
-  },
-  methods: {
-    toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen;
-    }
-  }
+  name: 'App'
 }
 </script>
 
