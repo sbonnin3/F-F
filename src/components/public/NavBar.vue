29cc31577db8e0eadc0c288e3887c00e93723ab5
Nathan BOSCHI <nathanboschi25@gmail.com>
2023-11-15 01:29:32 +0100

    nouvelle navbar

--- src/components/public/NavBar.vue
+++ src/components/public/NavBar.vue
@@ -1,36 +1,133 @@
 <template>
-    <div>
-        <nav class="navbar">
-            <div class="container">
-                <div class="mobile-header">
-                <a href="/" class="logo">
-                    <img src="@/assets/images/Image1.png" alt="Logo">
-                </a>
-                <button class="menu-button" @click="toggleMenu">&#9776;</button>
-                </div>
-                <div class="nav-buttons" :class="{ 'active': isMenuOpen }">
-                <router-link :to="{name: 'about'}" class="buttons-nav">A Propos</router-link>
-                <router-link :to="{name: 'activities'}" class="buttons-nav">Activités</router-link>
-                <router-link :to="{name: 'planning'}" class="buttons-nav">Planning</router-link>
-                <router-link :to="{name: 'providers'}" class="buttons-nav">Prestataires</router-link>
-                <router-link :to="{name: 'login'}" class="buttons-nav">Connexion</router-link>
-                </div>
-            </div>
-        </nav>
+  <nav>
+    <div class="brand">
+      <router-link :to="{ name: 'home' }">
+        <img src="@/assets/images/Image1.png" alt="Logo" />
+      </router-link>
     </div>
+    <div class="hamburger" @click="toggleMenu">
+      <i class="material-symbols">
+        {{ isMenuOpen ? "close" : "menu" }}
+      </i>
+    </div>
+    <div class="nav-links" :class="{ open: isMenuOpen }">
+      <router-link v-for="(link, id) in links" :key="id" :to="link.to">
+        {{ link.title }}
+      </router-link>
+    </div>
+  </nav>
 </template>
+
 <script>
 export default {
-    name: "PublicNav",
-    data() {
-        return {
-            isMenuOpen: false
-        };
+  props: {
+    links: {
+      type: Array,
+      required: true,
     },
-    methods: {
-        toggleMenu() {
-            this.isMenuOpen = !this.isMenuOpen;
-        }
-    }
-}
+  },
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
 </script>
+
+<style scoped lang="scss">
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 90px;
+  background-color: #000;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 20px;
+
+  z-index: 1000;
+
+  .brand {
+    img {
+      height: 65px;
+    }
+    z-index: 1001;
+  }
+
+  .hamburger {
+    display: none;
+    cursor: pointer;
+    color: #fff;
+
+    i {
+      font-size: 2.3rem;
+    }
+
+    z-index: 1001;
+  }
+
+  .nav-links {
+    display: flex;
+    align-items: center;
+
+    gap: 40px;
+
+    a {
+      color: #fff;
+      text-decoration: none;
+      font-size: 1.3rem;
+
+      padding: 6px 0;
+      border-bottom: transparent 3px solid;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        border-bottom: yellow 3px solid;
+      }
+    }
+    z-index: 1001;
+  }
+
+  @media screen and (max-width: 767px) {
+    .hamburger {
+      display: block;
+    }
+    .nav-links {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      background-color: #000;
+
+      transform: translateX(100%);
+      transition: all 0.3s ease-in-out;
+
+      a {
+        font-size: 2rem;
+        padding: 20px 0;
+      }
+
+      z-index: 1000;
+    }
+
+    .nav-links.open {
+      transform: translateX(0);
+    }
+  }
+}
+</style>
--- src/views/public/Template.vue
+++ src/views/public/Template.vue
@@ -1,17 +1,52 @@
 <template>
-    <div>
-        <PublicNav />
-        <router-view></router-view>
-    </div>
+  <div>
+    <PublicNav :links="publicNavLinks"/>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <script>
 import PublicNav from "@/components/public/NavBar.vue";
 
 export default {
-    name: "PublicTemplate",
-    components: {
-        PublicNav
-    }
-}
+  name: "PublicTemplate",
+  components: {
+    PublicNav,
+  },
+  data() {
+    return {
+      publicNavLinks: [
+        {
+          title: "Accueil",
+          to: { name: "home" },
+        },
+        {
+          title: "A propos",
+          to: { name: "about" },
+        },
+        {
+          title: "Prestataires",
+          to: { name: "providers" },
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+main {
+  /* To avoid the navbar to hide the content */
+  margin-top: 90px;
+
+  .container {
+    padding: 0 5%;
+
+    @media screen and (max-width: 768px) {
+      padding: 0;
+    }
+  }
+}
+</style>
