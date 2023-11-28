6a1a684aeb82fb6d18ae1c37d0f355596638a4ec
Manon <manon.grandmougin@edu.univ-fcomte.fr>
2023-11-28 02:33:11 +0100

    texte Page Accueil/A propos

--- src/views/public/Accueil.vue
+++ src/views/public/Accueil.vue
@@ -1,14 +1,14 @@
 <template class="page">
   <div class="content">
     <div class="background">
-      <h1>FAST & FABULOUS</h1>
-    </div>
-    <div class="titre">
-      <h1>Page d'accueil : Clique sur la 2ème carte tu verras c'est génial !</h1>
+      <h1> A propos : Notre événement </h1>
     </div>
     <div class="suite">
       <div class="text-image">
-        <p>Cypriis caesorumque has appulit Scironis Isauriae quae letalia ad navem quae quae sed funeribus appulit letalia</p>
+      <h2> 1. À propos du Grand Prix au Circuit Paul Ricard </h2>
+      <br>
+        <p>Le Grand Prix au Circuit Paul Ricard - un événement où la passion pour l'automobile et le désir de vivre des expériences uniques se rencontrent. Situé dans le sud de la France, le Circuit Paul Ricard est renommé pour sa piste impressionnante de 5,8 kilomètres, dotée de virages techniques et de longues lignes droites, offrant un terrain parfait pour les amateurs de vitesse. Notre événement s'adresse à tous : des passionnés de voitures classiques aux fans de technologie de pointe dans le sport automobile.</p>
+        <button class="more-info-button">En savoir plus</button>
       </div>
       <div class="image-container">
         <img src="@/assets/images/dessus_paul_ricard.jpg" alt="Circuit">
@@ -16,12 +16,22 @@
     </div>
     <div class="suite">
       <div class="image-container">
-        <a href="../page/carte" class="logo">
-          <img src="@/assets/images/carte.png" alt="Carte-3D">
-        </a>
+          <img src="voitures" alt="photo de différentes voitures">
       </div>
       <div class="text-image">
-        <p>Cypriis caesorumque has appulit Scironis Isauriae quae letalia ad navem quae quae sed funeribus appulit letalia</p>
+      <h3> 2. Notre Philosophie : Passion et Diversité </h3>
+      <br>
+        <p>La passion pour les voitures et le sport automobile est au cœur de notre événement. Nous croyons que chaque visiteur, qu'il soit un amateur ou un expert, mérite de vivre une expérience exceptionnelle. C'est pourquoi notre Grand Prix propose une variété d'activités - du karting au baptême de pilotage - pour que chacun puisse trouver son bonheur.</p>
+      </div>
+    </div>
+    <div class="suite">
+      <div class="text-image">
+      <h3> 3. Une Journée Inoubliable Pour Tous </h3>
+      <br>
+      <p> Notre objectif est de créer une ambiance inoubliable, où le frisson de la vitesse se mêle à l'excitation de la compétition. Des courses époustouflantes, des activités interactives, et des moments de détente sont soigneusement planifiés pour garantir une expérience complète. Que vous veniez en famille, entre amis ou en solo, le Grand Prix au Circuit Paul Ricard est l'endroit idéal pour partager votre passion et créer des souvenirs mémorables. </p>
+    </div>
+     <div class="image-container">
+        <img src="photo3.png" alt="podium">
       </div>
     </div>
   </div>
@@ -30,19 +40,17 @@
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Accueil'
+  name: 'A Propos'
 }
 </script>
 
 <style scoped>
 .background {
+  padding-top: 5%;
   position: relative;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #fff;
-  background-image: url('@/assets/images/paul_ricard.JPG');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -50,11 +58,13 @@ export default {
 
 .suite {
   padding-top: 5%;
-  padding-left: 20%;
-  padding-right: 20%;
+  padding-left: 10%;
+  padding-right: 10%;
   display: flex;
   justify-content: center;
   align-items: center;
+  line-height: 1.5;
+  font-size: 20px;
 }
 
 .image-container img {
@@ -69,4 +79,9 @@ export default {
 .text-image {
   padding: 10%;
 }
+
+.more-info-button:hover {
+  background-color: #0056b3;
+}
+
 </style>
--- src/views/public/HomeView.vue
+++ src/views/public/HomeView.vue
@@ -1,29 +1,83 @@
-<template>
+<template class="page">
   <div class="content">
-    <img class="affiche" src="@/assets/images/affiche.png" alt="Carte-3D">
+    <div class="background">
+      <h1 class="page-title">FAST & FABULOUS  </h1>   
+    </div>
+
+    <div class="titre">
+      <h1> Grand Prix Automobile au Circuit Paul Ricard </h1>
+    </div>
+    <div class="suite">
+      <div class="text">
+        <h1> Vivez une journée exceptionnelle de vitesse et d'adrénaline ! </h1>
+        <br>
+        <p> Le Circuit Paul Ricard est ravi d'accueillir l'édition 2024 de notre Grand Prix Automobile, une célébration palpitante pour les amateurs de vitesse et de performances mécaniques. Cet événement unique en son genre vous offre une journée complète de courses époustouflantes, d'activités passionnantes et de divertissements pour tous les âges. </p>
+        <br> <br>
+        <h2> Points forts de l'événement :</h2>
+        <br>
+        <ul>
+        <li> Le Grand Prix : L'attraction principale de la journée, notre Grand Prix vous promet des courses à couper le souffle avec certains des meilleurs pilotes du circuit. Vivez l'excitation de la vitesse et la compétition sur l'un des circuits les plus emblématiques de France. </li>
+        <li>Karting pour tous : Que vous soyez débutant ou pilote expérimenté, profitez de nos sessions de karting. Une occasion parfaite pour sentir l'adrénaline d'une course et pourquoi pas, découvrir de futurs talents. </li>
+        <li> Baptêmes de pilotage : Vivez le frisson de la vitesse en passager d'une voiture de course pilotée par des professionnels. Une expérience inoubliable qui vous fera vivre les sensations d'une vraie course </li>
+        <li> Expositions et animations : Découvrez une gamme de voitures classiques et modernes, participez à des ateliers interactifs et profitez d'animations pour toute la famille.</li>
+        <li> Restauration et zones de détente : Des espaces de restauration seront disponibles pour vous permettre de recharger vos batteries et de profiter de la journée confortablement. </li>
+        </ul>
+        <br>
+        <h3> Rejoignez-nous pour une journée mémorable au cœur de l'action et de la passion automobile. Le Grand Prix au Circuit Paul Ricard est plus qu'un événement, c'est une expérience à vivre pleinement !</h3>
+      </div>
+    </div>
   </div>
+ 
 </template>
 
 <script>
 export default {
   name: 'HomeView'
 }
+
 </script>
 
 <style scoped>
 body {
   margin: 0;
-  overflow: hidden; /* Empêche le défilement de la page */
+}
+
+.background {
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url('@/assets/images/paul_ricard.JPG');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.suite{
+  padding-top: 5%;
+  padding-left: 20%;
+  padding-right: 20%;
+  padding-bottom:20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.5;
+  font-size: 20px;
 }
 
 .content {
   width: 100%;
   height: 100vh; /* Assurez-vous que la hauteur de votre contenu occupe toute la vue de la fenêtre */
-  overflow: hidden;
-  background: url("@/assets/images/video.gif") center center fixed;
+  /*background: url("@/assets/images/video.gif") center center fixed;     = animation voiture*/
   background-size: cover;
   margin: 0;
   padding: 0;
+}
+
+.titre {
+  padding-top: 1%;
+  text-align: center;
 }
 
 .affiche {
