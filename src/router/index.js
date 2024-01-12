9d14b2ee1ed6f295f9a172c45bb3329b87b2afb0
Nathan BOSCHI <nathanboschi25@gmail.com>
2024-01-12 08:58:22 +0100

    déplacement du formulaire pour avoir le menu sur le formulaire de reservation

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Formulaire from '@/views/public/Formulaire.vue';
-
 import publicRoutes from '@/router/public.router'
 import dashboardRoutes from '@/router/dashboard.router'
 
@@ -11,17 +9,6 @@ Vue.use(VueRouter)
 const routes = [
     ...publicRoutes,
     ...dashboardRoutes,
-    {
-        path: '/reservation-form',
-        name: 'ReservationForm',
-        component: Formulaire,
-        props: route => ({ 
-            selectedActivity: route.query.selectedActivity, 
-            selectedTimeSlot: route.query.selectedTimeSlot 
-        })
-    },
-    
-
 ];
 
 const router = new VueRouter({
--- src/router/public.router.js
+++ src/router/public.router.js
@@ -1,58 +1,67 @@
 const publicRoutes = [
-    {
-        path: "/",
-        component: () => import("@/views/public/Template.vue"),
+  {
+    path: "/",
+    component: () => import("@/views/public/Template.vue"),
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () => import("@/views/public/HomeView.vue"),
+      },
+      {
+        path: "map",
+        name: "map",
+        component: () => import("@/views/public/map/ThreeD.vue"),
+      },
+      {
+        path: "activities",
+        name: "activities",
+        component: () => import("@/views/public/Activites.vue"),
+      },
+      {
+        path: "providers",
+        name: "providers",
+        component: () => import("@/views/public/Prestataire.vue"),
         children: [
-            {
-                path: "",
-                name: "home",
-                component: () => import("@/views/public/HomeView.vue"),
-            },
-            {
-                path: "map",
-                name: "map",
-                component: () => import("@/views/public/map/ThreeD.vue"),
-            },
-            {
-                path: "activities",
-                name: "activities",
-                component: () => import("@/views/public/Activites.vue"),
-            },
-            {
-                path: "providers",
-                name: "providers",
-                component: () => import("@/views/public/Prestataire.vue"),
-                children: [
-                    {
-                        path: ":id(\\d+)",
-                        name: "provider",
-                        component: () => import("@/components/public/ProviderProfile.vue"),
-                        props: true,
-                    },
-                ],
-            },
-            {
-                path: '/myaccount',
-                name: 'myAccount',
-                component: () => import('@/views/MyAccount.vue')
-            },
-            {
-                path: '/login',
-                name: 'login',
-                component: () => import('@/views/Login.vue')
-            },
-            {
-                path: '/logout',
-                name: 'logout',
-                component: () => import('@/views/Logout.vue')
-            },
-            {
-                path: "/reservation",
-                name: "reservation",
-                component: () => import("@/views/public/Reservation.vue"),
-            },
+          {
+            path: ":id(\\d+)",
+            name: "provider",
+            component: () => import("@/components/public/ProviderProfile.vue"),
+            props: true,
+          },
         ],
-    },
+      },
+      {
+        path: "/myaccount",
+        name: "myAccount",
+        component: () => import("@/views/MyAccount.vue"),
+      },
+      {
+        path: "/login",
+        name: "login",
+        component: () => import("@/views/Login.vue"),
+      },
+      {
+        path: "/logout",
+        name: "logout",
+        component: () => import("@/views/Logout.vue"),
+      },
+      {
+        path: "/reservation",
+        name: "reservation",
+        component: () => import("@/views/public/Reservation.vue"),
+      },
+      {
+        path: "/reservation-form",
+        name: "ReservationForm",
+        component: import("@/views/public/Formulaire.vue"),
+        props: (route) => ({
+          selectedActivity: route.query.selectedActivity,
+          selectedTimeSlot: route.query.selectedTimeSlot,
+        }),
+      },
+    ],
+  },
 ];
 
 export default publicRoutes;
